e2cb6c2a2364b913f4495239401f5aed6517d671
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-01-30 10:28:36 +0200

    0x1D. C - Binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -35,3 +35,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (new_binary_tree_height(tree->left)
 		- new_binary_tree_height(tree->right));
 }
+
+/**
+ * binary_tree_height - Write function that measures the height of binary tree
+ * @tree: tree to check
+ * Return: height of tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lefttt = 0;
+	size_t righttt = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			lefttt = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			righttt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((lefttt > righttt) ? lefttt : righttt);
+	}
+}
